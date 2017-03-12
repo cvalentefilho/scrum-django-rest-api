9d18982b1dd1a7a5f96d62160313d29b37dcc8b5
Hal-9000 <hall-9000@localhost.localdomain>
2017-03-12 20:05:35 -0300

    Creating taks models and adjustments in .gitiginore

--- board/models.py
+++ board/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,3 +14,32 @@ class Sprint(models.Model):
 
     def _str__(self):
         return self.name or _('Sprint ending %s') % self.end
+
+
+class TaskModel(models.Model):
+    """Unidade referente ao trabalho a ser realizado na sprint"""
+
+    STATUS_TODO = 1
+    STATUS_IN_PROGRESS = 2
+    STATUS_TESTING = 3
+    STATUS_DONE = 4
+
+    STATUS_CHOICES = (
+        (STATUS_TODO, _('Not Started')),
+        (STATUS_IN_PROGRESS, _('In Progress')),
+        (STATUS_TESTING, _('Testing')),
+        (STATUS_DONE, _('Done')),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+    sprint = models.ForeignKey(Sprint, blank=True, null=True)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, default=STATUS_TODO)
+    order = models.SmallIntegerField(default=0)
+    assigned = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    started = models.DateField(blank=True, null=True)
+    due = models.DateField(blank=True, null=True)
+    completed = models.DateField(blank=True, null=True)
+
+    def _str__(self):
+        return self.name
